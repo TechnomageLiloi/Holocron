74a310e21555856fd7b6babb2beee1bc71166429
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-06-13 09:39:10 +0300

    [0.0.11] Add getting children.

--- Source/Domain/Sphere/Entity.php
+++ Source/Domain/Sphere/Entity.php
@@ -5,6 +5,10 @@ namespace Liloi\Holocron\Domain\Sphere;
 use Liloi\Stylo\Parser;
 use Liloi\Tools\Entity as AbstractEntity;
 
+/**
+ * @method string getPath(): string
+ * @method void setPath(string $value)
+ */
 class Entity extends AbstractEntity
 {
 
--- Source/Types/Node/Entity.php
+++ Source/Types/Node/Entity.php
@@ -3,8 +3,45 @@
 namespace Liloi\Holocron\Types\Node;
 
 use Liloi\Holocron\Domain\Sphere\Entity as AbstractEntity;
+use Liloi\Holocron\Domain\Sphere\Collection;
+use Liloi\Holocron\Types\Atom\Entity as AtomEntity;
 
 class Entity extends AbstractEntity
 {
+    public function getChildren(): Collection
+    {
+        $collection = new Collection();
 
+        $parent = $this->getPath();
+
+        if ($handle = opendir($parent))
+        {
+            while (false !== ($entry = readdir($handle)))
+            {
+                if (in_array($entry, ['.', '..']))
+                {
+                    continue;
+                }
+
+                $child = $parent . '/' . $entry;
+                $parts = pathinfo($child);
+
+                $data = [
+                    'path' => $child
+                ];
+
+                if(is_dir($child))
+                {
+                    $collection[] = self::create($data);
+                    continue;
+                }
+
+                $collection[] = AtomEntity::create($data);
+            }
+
+            closedir($handle);
+        }
+
+        return $collection;
+    }
 }
